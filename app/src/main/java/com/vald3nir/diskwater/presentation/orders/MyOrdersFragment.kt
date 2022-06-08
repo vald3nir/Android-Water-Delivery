ff594d9bd631b7c6adea6b2bedb6bdd3bcd1213e
Valdenir Severino <vald3nir@gmail.com>
2022-06-08 16:44:57 -0300

    init my orders

--- app/src/main/java/com/vald3nir/diskwater/presentation/orders/MyOrdersFragment.kt
+++ app/src/main/java/com/vald3nir/diskwater/presentation/orders/MyOrdersFragment.kt
@@ -4,14 +4,43 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.vald3nir.diskwater.R
 import com.vald3nir.diskwater.common.BaseFragment
+import com.vald3nir.diskwater.data.dto.ProductDTO
 import com.vald3nir.diskwater.databinding.FragmentMyOrdersBinding
+import com.vald3nir.diskwater.databinding.ProductItemViewBinding
+import com.vald3nir.toolkit.componets.adapters.CustomListAdapterDiffer
+import com.vald3nir.toolkit.data.dto.BaseDTO
+import com.vald3nir.toolkit.data.dto.baseDiffUtil
+import com.vald3nir.toolkit.extensions.setupToolbar
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MyOrdersFragment : BaseFragment() {
 
     private val viewModel: OrderViewModel by viewModel()
     lateinit var binding: FragmentMyOrdersBinding
+    private val mainCardAdapter by lazy {
+        val adapter = CustomListAdapterDiffer(
+            bindingInflater = ProductItemViewBinding::inflate,
+            list = listOf(),
+            itemDiffUtil = baseDiffUtil(),
+            ::bindAdapter
+        )
+        adapter
+    }
+
+    private fun bindAdapter(
+        baseDTO: BaseDTO,
+        itemViewBinding: ProductItemViewBinding,
+        position: Int,
+        any: Any
+    ) {
+        if (baseDTO is ProductDTO) {
+            itemViewBinding.apply {
+
+            }
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,12 +54,39 @@ class MyOrdersFragment : BaseFragment() {
     private fun initViews() {
         viewModel.appView = appView
         binding.apply {
-
+            toolbar.setupToolbar(
+                activity = activity,
+                title = getString(R.string.my_last_orders),
+                showBackButton = false,
+            )
         }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupObservers()
+        loadLastOrders()
+    }
 
+    private fun loadLastOrders() {
+        binding.btnNewOrder.showLoading(true)
+        viewModel.loadLastOrders()
+    }
+
+    private fun setupObservers() {
+
+//        binding.btnAddProducts.setOnClickListener {
+//            viewModel.replaceFragment(FragmentEnum.PRODUCT_DETAIL)
+//        }
+//
+//        mainCardAdapter.setOnItemClickListener(listener = { item, pos ->
+//            viewModel.replaceFragment(FragmentEnum.PRODUCT_DETAIL, item)
+//        })
+//
+//        viewModel.products.observe(viewLifecycleOwner) {
+//            mainCardAdapter.submitList(it.toMutableBaseList())
+//            binding.clcOrdersOpen.notifyListSize(it.size)
+//            binding.btnAddProducts.showLoading(false)
+//        }
     }
 }
--- app/src/main/java/com/vald3nir/diskwater/presentation/orders/OrderViewModel.kt
+++ app/src/main/java/com/vald3nir/diskwater/presentation/orders/OrderViewModel.kt
@@ -3,4 +3,8 @@ package com.vald3nir.diskwater.presentation.orders
 import com.vald3nir.toolkit.core.CoreViewModel
 
 class OrderViewModel() : CoreViewModel() {
+
+    fun loadLastOrders() {
+
+    }
 }
